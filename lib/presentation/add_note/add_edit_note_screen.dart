a9c317f7e3332a46d44b86b3219f0d82025e3ccd
Chow Minh Khang <minhkhang@lozi.vn>
2024-03-11 01:10:27 +0700

    add delete feature

--- lib/presentation/add_note/add_edit_note_screen.dart
+++ lib/presentation/add_note/add_edit_note_screen.dart
@@ -39,47 +39,92 @@ class AddEditNoteScreen extends StatelessWidget {
                   children: [
                     const SizedBox(height: 10),
                     Text(
-                      'Created date: ${note != null ? DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.fromMillisecondsSinceEpoch(note!.createdAt)) : ''}',
+                      'Last update: ${note != null ? DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.fromMillisecondsSinceEpoch(note!.createdAt)) : ''}',
                     )
                   ],
                 ),
               ),
               const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  if (titleController.text.trim().isEmpty) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Title can not be empty!'),
-                        behavior: SnackBarBehavior.floating,
-                      ),
-                    );
-                    return;
-                  }
-                  if (note == null) {
-                    noteDao.insertNote(
-                      Note(
-                        title: titleController.text.trim(),
-                        description: descriptionController.text.trim(),
-                        createdAt: DateTime.now().millisecondsSinceEpoch,
-                      ),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Inserted new note.'),
-                        behavior: SnackBarBehavior.floating,
-                      ),
-                    );
-                  } else {
-                    note!.title = titleController.text.trim();
-                    note!.description = descriptionController.text.trim();
-                    note!.createdAt = DateTime.now().millisecondsSinceEpoch;
-                    noteDao.updateNote(note!);
-                  }
-                  Navigator.of(context).pop();
-                },
-                child: Text(note == null ? 'Add' : 'Edit'),
-              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (titleController.text.trim().isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Title can not be empty!'),
+                            behavior: SnackBarBehavior.floating,
+                          ),
+                        );
+                        return;
+                      }
+                      if (note == null) {
+                        noteDao.insertNote(
+                          Note(
+                            title: titleController.text.trim(),
+                            description: descriptionController.text.trim(),
+                            createdAt: DateTime.now().millisecondsSinceEpoch,
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Inserted new note.'),
+                            behavior: SnackBarBehavior.floating,
+                          ),
+                        );
+                      } else {
+                        note!.title = titleController.text.trim();
+                        note!.description = descriptionController.text.trim();
+                        note!.createdAt = DateTime.now().millisecondsSinceEpoch;
+                        noteDao.updateNote(note!);
+                      }
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(note == null ? 'Add' : 'Edit'),
+                  ),
+                  Visibility(
+                    visible: note != null,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: const Text(
+                                  'Do you really want to delete this note?'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    noteDao.deleteNote(note!);
+                                    Navigator.of(context).pop();
+                                    Navigator.of(context).pop();
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text('Deleted!'),
+                                        behavior: SnackBarBehavior.floating,
+                                      ),
+                                    );
+                                  },
+                                  child: const Text('Yes'),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    // Close the dialog when the "Close" button is pressed
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('No'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: const Text('Delete'),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
